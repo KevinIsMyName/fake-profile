eeff6544fee448372fd5fc5622d9ec28950e381a
KevinIsMyName <wu.kevin.2520@gmail.com>
2020-12-26 16:31:38 -0500

    Attempted a Bootstrap card, failed miserably

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import './Card.css';
 import KanyeApi from './Api/KanyeApi';
 import Person from './Api/Person';
-import Image from 'react-bootstrap/Image'
-import Badge from 'react-bootstrap/Badge'
+import Image from 'react-bootstrap/Image';
+import Badge from 'react-bootstrap/Badge';
+import { Card as BootstrapCard } from 'react-bootstrap';
 export default class Card extends Component {
   constructor(props) {
     super(props);
@@ -49,16 +50,18 @@ export default class Card extends Component {
             <h1><Badge variant="light">{this.state.person.name.first}</Badge> {this.state.person.name.last}</h1>
           </div>
           <div className="description">
-            {this.state.person.name.title} {this.state.person.name.first} {this.state.person.name.last} currently lives
-             at {this.state.person.location.street.number} {this.state.person.location.street.name},
-             {this.state.person.location.city}, {this.state.person.location.state}, {this.state.person.location.country}.
-              You can contact {this.state.person.gender === 'male' ? 'him' : 'her'} through email at {this.state.person.email} 
-              or {this.state.person.gender === 'male' ? 'his' : 'her'} phone at {this.state.person.phone}. {this.state.person.gender === 'male' ? 'He' : 'She'} enjoys going to {this.state.person.location.timezone.description} 
-              for vacation.
+            {this.state.person.name.title} {this.state.person.name.first} {this.state.person.name.last} currently lives in {this.state.person.location.city}, {this.state.person.location.state}, {this.state.person.location.country}. You can contact {this.state.person.gender === 'male' ? 'him' : 'her'} through email at {this.state.person.email} or {this.state.person.gender === 'male' ? 'his' : 'her'} phone at {this.state.person.phone}. {this.state.person.gender === 'male' ? 'He' : 'She'} enjoys going to {this.state.person.location.timezone.description} for vacation.
           </div>
           <div className="quote">
             {this.state.quote} —{this.state.person.name.first} {this.state.person.name.last}
           </div>
+          {/* <BootstrapCard style={{ width: '18rem' }}>
+            <BootstrapCard.Img variant="top" roundedCircle src={this.state.person.picture.large}></BootstrapCard.Img>
+            <BootstrapCard.Body>
+              <BootstrapCard.Title>{this.state.person.name.first} {this.state.person.name.last}</BootstrapCard.Title>
+              BootstrapCard.Body
+            </BootstrapCard.Body>
+          </BootstrapCard> */}
         </div>
       );
     }
