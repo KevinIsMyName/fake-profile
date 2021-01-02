bc3120fc4d7e0f94d7c496a0818e90eac962c446
KevinIsMyName <wu.kevin.2520@gmail.com>
2021-01-01 22:02:11 -0500

    About to install cli-plugin-eslint

---
+++ src/components/Card.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="card">
+        <img src="https://www.google.com/logos/doodles/2021/new-years-day-2021-6753651837108820-law.gif"></img>
+        <div>{{firstName}} {{lastName}}</div>
+        <div>{{description}}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import axios from "axios";
+
+export default {
+  name: "Card",
+  props: {},
+  setup() {
+    const quote = ref(null);
+
+    function getQuote() {
+      return "Quote.";
+    }
+
+    onMounted(() => {
+      getQuote();
+    });
+
+    return {
+      quote,
+    };
+  },
+  async beforeMount() {
+    console.log("Before mount.");
+    await axios.get("https://api.kanye.rest").then((response) => {
+      this.quote = response;
+    });
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  border: 1px solid red;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <Card />
+    <Card />
+    <Card />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Card from "@/components/Card.vue"; // @ is an alias to /src
+
+@Component({
+  components: { Card },
+})
+export default class Home extends Vue {}
+</script>
+
+<style scoped>
+.home {
+  border: 1px dashed red;
+}
+</style>
